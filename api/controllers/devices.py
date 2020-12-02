9ae11eb826fec62dfe8f0f5c5c998d2ae82d2eff
Danilo Peixoto Ferreira <danilo.ferreira@inatel.br>
2020-12-02 15:35:56 -0300

    Fix minor issue

--- api/controllers/devices.py
+++ api/controllers/devices.py
@@ -10,14 +10,14 @@ def join(context, device):
   address = cluster.get_address()
   token = cluster.join(device.dict())
 
-  device_document = DeviceDocument(**device.dict())
+  device_document = DeviceDocument(
+    **device.dict()
+    cluster_address = address,
+    token = token)
 
   database.save(device_document, 'devices')
 
-  return DeviceResponse(
-    **device_document.dict(),
-    cluster_address = address,
-    token = token)
+  return DeviceResponse(**device_document.dict())
 
 def get(context, device_id):
   database = context['database']
