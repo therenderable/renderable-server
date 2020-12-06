adf9a8d61f855265b2f00d60531d9cd9bbe76edb
Danilo Peixoto Ferreira <danilo.ferreira@inatel.br>
2020-12-05 21:54:08 -0300

    Fix device controller

--- api/controllers/devices.py
+++ api/controllers/devices.py
@@ -10,20 +10,28 @@ def join(context, device):
   address = cluster.get_address()
   token = cluster.join(device.dict())
 
-  device_document = DeviceDocument(
-    **device.dict(),
-    cluster_address = address,
-    token = token)
+  device_document = DeviceDocument(**device.dict())
 
   database.save(device_document, 'devices')
 
-  return DeviceResponse(**device_document.dict())
+  return DeviceResponse(
+    **device_document.dict(),
+    cluster_address = address,
+    token = token)
 
 def get(context, device_id):
+  cluster = context['cluster']
   database = context['database']
+
   device_document = database.find(device_id, 'devices')
 
   if device_document is None:
     raise exceptions.invalid_resource_id
 
-  return DeviceResponse(**device_document.dict())
+  address = cluster.get_address()
+  token = cluster.join(device_document.dict())
+
+  return DeviceResponse(
+    **device_document.dict(),
+    cluster_address = address,
+    token = token)
--- api/routers/devices.py
+++ api/routers/devices.py
@@ -20,10 +20,9 @@ def join_device(request: Request, device: DeviceRequest, api_key: auth.APIKey = 
   return devices.join(request.app.state.context, device)
 
 responses = {
-  exceptions.invalid_api_key.status_code: {'model': ErrorResponse},
   exceptions.invalid_resource_id.status_code: {'model': ErrorResponse}
 }
 
 @router.get('/{device_id}', response_model = DeviceResponse, responses = responses, response_model_exclude_unset = True)
-def get_device_by_id(request: Request, device_id: ObjectID, api_key: auth.APIKey = auth.get_api_key()):
+def get_device_by_id(request: Request, device_id: ObjectID):
   return devices.get(request.app.state.context, device_id)
